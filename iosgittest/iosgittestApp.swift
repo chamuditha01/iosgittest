7bbb3edb6377be53719f50d1d8051c264b9876ca
chamuditha01 <heshanchamuditha05@gmail.com>
2026-02-26 13:08:32 +0530

    Initial Commit

---
+++ iosgittest/iosgittestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosgittestApp.swift
+//  iosgittest
+//
+//  Created by Dileesha on 2026-02-26.
+//
+
+import SwiftUI
+
+@main
+struct iosgittestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
